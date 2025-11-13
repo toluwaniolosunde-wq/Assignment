d8a927b680dbf57916b73513df80b5d904c76bdc
toluwaniolosunde-wq <toluwaniolosunde@gmail.com>
2025-11-13 14:00:40 +0100

    Add files via upload

---
+++ Tolu.py
@@ -0,0 +1,19 @@
+name= "Tolu"
+number= 12
+amount=1000
+print(len(name))
+print(len(number))
+print(len(amount))
+
+
+print("=" * 25)
+print((number)** 0.5)
+print("=" *25)
+
+
+
+
+print("This is ade's bike")
+print('This is ade\'s bike')
+print("The hot tea\\ bread")
+
