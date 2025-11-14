bb2ef50c7d1ddff4e6f4a5dc141c6dbc8bb1238b
toluwaniolosunde-wq <toluwaniolosunde@gmail.com>
2025-11-14 04:12:49 +0100

    Add files via upload

---
+++ Investmentreturn.py
@@ -0,0 +1,13 @@
+p= 1000
+r= 0.07
+
+after_ten_years= p*(1+r) ** 10
+after_twenty_years= p*(1+r) ** 20
+after_thirty_years= p*(1+r) ** 30
+
+print(f"Amount after 10 years: ${after_ten_years: }")
+print(f"Amount after 20 years: ${after_twenty_years: }")
+print(f"Amount after 30 years: ${after_thirty_years: }")
+
+
+
---
+++ SquareCube.py
@@ -0,0 +1,7 @@
+print("number\tsquare\tcube")
+print("0\t0\t0")
+print("1\t1\t1")
+print("02\t4\t8")
+print("3\t9\t27")
+print("4\t16\t64")
+print("5\t25\t125")
